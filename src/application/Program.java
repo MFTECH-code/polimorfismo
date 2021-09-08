38a28790d6fd9e9efbf7a2c12dba651e3a1191f1
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-09-07 23:57:27 -0300

    Finalização do projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,57 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
 public class Program {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws ParseException {
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		List<Product> products = new ArrayList<>();
+		
+		System.out.print("Enter the number of products: ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println("Product #" + (i + 1) + " data:");
+			System.out.print("Common, used or imported (c/u/i)? ");
+			char tag = sc.next().charAt(0);
+			System.out.print("Name: ");
+			String name = sc.next();
+			System.out.print("Price: ");
+			double price = sc.nextDouble();
+			if (tag == 'u') {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.next());
+				Product product = new UsedProduct(name, price, manufactureDate);
+				products.add(product);
+			} else if (tag == 'i') {
+				System.out.print("Customs fee: ");
+				double customFee = sc.nextDouble();
+				Product product = new ImportedProduct(name, price, customFee);
+				products.add(product);
+			} else {
+				Product product = new Product(name, price);
+				products.add(product);
+			}
+		}
+		
+		System.out.println();
+		System.out.println("PRICE TAGS: ");
+		for (Product product : products) {
+			System.out.println(product.priceTag());
+		}
+		
+		sc.close();
 	}
 
 }
